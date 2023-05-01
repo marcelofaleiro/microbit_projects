400ed2c02498cb2553dce61e304b326a8690c36e
Marcelo Carvalho Faleiro de Almeida <mcfa85@gmail.com>
2023-05-01 20:22:02 +0200

    Update temperature_sensor.py
    fix

--- temperature_sensor.py
+++ temperature_sensor.py
@@ -2,12 +2,8 @@
 from microbit import *
 
 # Define the on_button_pressed_a function
-def on_button_pressed_a():
-    display.scroll(str(temperature()))
 
 # Code in a 'while True:' loop repeats forever
 while True:
-    if button_a.is_pressed():
-        on_button_pressed_a()
     display.show(Image.ANGRY)
     sleep(1000)
